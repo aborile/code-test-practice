71232a956105e456fdef6ecef8e8c0a968d30136
aborile <37267486+aborile@users.noreply.github.com>
2024-02-11 00:20:20 +0900

    Create 달리기-경주.js

---
+++ 달리기-경주.js
@@ -0,0 +1,18 @@
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/178871
+ * 코딩테스트 연습 > 연습문제 > 달리기 경주
+ */
+
+function solution(players, callings) {
+    const ranks = players.slice();
+    const indexMap = new Map();
+    callings.forEach((p) => {
+        const index = indexMap.get(p);
+        const rank = index ?? ranks.findIndex((r) => r === p);
+        ranks[rank] = ranks[rank - 1];
+        indexMap.set(ranks[rank - 1], rank);
+        ranks[rank - 1] = p;
+        indexMap.set(p, rank -1 );
+    });
+    return ranks;
+}
